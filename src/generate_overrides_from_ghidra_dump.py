e50a75d2baf57dca9019204e06e7ff82dbae3c73
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-23 15:53:34 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-23

--- src/generate_overrides_from_ghidra_dump.py
+++ src/generate_overrides_from_ghidra_dump.py
@@ -4,8 +4,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 22 июня 2026 06:51:13
-# Version: 1.0.78
+# Last Updated: 23 июня 2026 15:53:34
+# Version: 1.0.79
 # ========================================
 import re
 import sys
