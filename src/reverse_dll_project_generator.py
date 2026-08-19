54d553bcddfcf6ced63b3bc8125ef4fed1595d96
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-19 10:21:03 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-19

--- src/reverse_dll_project_generator.py
+++ src/reverse_dll_project_generator.py
@@ -4,8 +4,8 @@
 # Author: Nikolay Dvurechensky
 # Site: https://dvurechensky.pro/
 # Gmail: dvurechenskysoft@gmail.com
-# Last Updated: 18 августа 2026 06:51:14
-# Version: 1.0.136
+# Last Updated: 19 августа 2026 10:21:03
+# Version: 1.0.137
 # ========================================
 """
 reverse_dll_project_generator.py
